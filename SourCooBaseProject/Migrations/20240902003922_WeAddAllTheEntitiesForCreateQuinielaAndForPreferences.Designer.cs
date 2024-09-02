d9e681f482350ec27a387edb9ba6604fff942dbf
Iván Ramírez <ivaramcer@gmail.com>
2024-09-01 18:41:25 -0600

    Migration added for the entities

--- SourCooBaseProject/Migrations/20240902003922_WeAddAllTheEntitiesForCreateQuinielaAndForPreferences.Designer.cs
+++ SourCooBaseProject/Migrations/20240902003922_WeAddAllTheEntitiesForCreateQuinielaAndForPreferences.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using QuinielasApi.Models.DatabaseContext;
@@ -11,9 +12,11 @@ using QuinielasApi.Models.DatabaseContext;
 namespace QuinielasApi.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240902003922_WeAddAllTheEntitiesForCreateQuinielaAndForPreferences")]
+    partial class WeAddAllTheEntitiesForCreateQuinielaAndForPreferences
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
