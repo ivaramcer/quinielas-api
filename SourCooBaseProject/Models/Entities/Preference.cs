90531ec4238591e314aff15129e613ed5cb7fe78
Iván Ramírez <ivaramcer@gmail.com>
2024-10-06 16:59:02 -0600

    preferences were updated!

--- SourCooBaseProject/Models/Entities/Preference.cs
+++ SourCooBaseProject/Models/Entities/Preference.cs
@@ -6,10 +6,11 @@ namespace QuinielasApi.Models.Entities
     {
         [Key]
         public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; } = default!;
         public int SportId { get; set; }
         public Sport Sport { get; set; } = default!;
-        public int PersonId { get; set; }
-        public Person Person { get; set; } = default!;
+
         public int? NFLTeamId { get; set; }
         public NFLTeam? NFLTeam { get; set; } = default!;
         public int? SoccerTeamId { get; set; }
