5c178b29251b00a62307cb0da2926e72d75b64a4
Iván Ramírez <ivaramcer@gmail.com>
2024-10-23 13:59:32 -0600

    Migration for UserId to walet

---
+++ SourCooBaseProject/Migrations/20241023195753_WalletStoresTheUserId.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuinielasApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class WalletStoresTheUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Wallets",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wallets_UserId",
+                table: "Wallets",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallets_Users_UserId",
+                table: "Wallets",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallets_Users_UserId",
+                table: "Wallets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Wallets_UserId",
+                table: "Wallets");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Wallets");
+        }
+    }
+}
