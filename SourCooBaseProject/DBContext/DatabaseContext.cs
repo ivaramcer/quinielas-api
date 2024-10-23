f72a4181b9cca181c36ffd5d2c4f628a03e703d4
Iván Ramírez <ivaramcer@gmail.com>
2024-10-22 22:55:12 -0600

    DBContext configured!

--- SourCooBaseProject/DBContext/DatabaseContext.cs
+++ SourCooBaseProject/DBContext/DatabaseContext.cs
@@ -30,25 +30,6 @@ namespace QuinielasApi.DBContext
             .IsUnique();
             
 
-            // RELATIONS
-            /*modelBuilder.Entity<Team>()
-                       .HasMany(t => t.HomeGames)
-                       .WithOne(g => g.HomeTeam)
-                       .HasForeignKey(g => g.HomeTeamId)
-                       .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Team>()
-                .HasMany(t => t.AwayGames)
-                .WithOne(g => g.AwayTeam)
-                .HasForeignKey(g => g.AwayTeamId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Team>()
-                .HasMany(t => t.WinnerGames)
-                .WithOne(g => g.WinnerTeam)
-                .HasForeignKey(g => g.WinnerTeamId)
-                .OnDelete(DeleteBehavior.Restrict);*/
-
             modelBuilder.Entity<Game>()
                 .HasOne(g => g.WinnerTeam)
                 .WithMany()
@@ -57,29 +38,27 @@ namespace QuinielasApi.DBContext
             
 
         }
-
-        public DbSet<Person> Persons { get; set; } = default!;
-        public DbSet<User> Users { get; set; } = default!;
-        public DbSet<Role> Roles { get; set; } = default!;
-        public DbSet<Permission> Permissions { get; set; } = default!;
-        public DbSet<Sport> Sports { get; set; } = default!;
-        public DbSet<OperationType> OperationTypes { get; set; } = default!;
-        public DbSet<Preference> Preferences { get; set; } = default!;
-        public DbSet<Quiniela> Quinielas { get; set; } = default!;
-        public DbSet<QuinielaConfiguration> QuinielaConfigurations { get; set; } = default!;
-        public DbSet<QuinielaType> QuinielaTypes { get; set; } = default!;
-        public DbSet<QuinielaTypeConfiguration> QuinielaTypeConfigurations { get; set; } = default!;
-        public DbSet<QuinielaGame> QuinielaGames { get; set; }
-
-        public DbSet<Status> Status { get; set; } = default!;
-
-
-        public DbSet<League> Leagues { get; set; } = default!;
-        public DbSet<Team> Teams { get; set; }
         public DbSet<Game> Games { get; set; }
-
-        
-        
-
+        public DbSet<Gamepass> Gamepasses { get; set; }
+        public DbSet<League> Leagues { get; set; }
+        public DbSet<OperationType> OperationTypes { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Preference> Preferences { get; set; }
+        public DbSet<Quiniela> Quinielas { get; set; }
+        public DbSet<QuinielaConfiguration> QuinielaConfigurations { get; set; }
+        public DbSet<QuinielaDuration> QuinielaDurations { get; set; }
+        public DbSet<QuinielaGame> QuinielaGames { get; set; }
+        public DbSet<QuinielaPickDuration> QuinielaPickDurations { get; set; }
+        public DbSet<QuinielaType> QuinielaTypes { get; set; }
+        public DbSet<QuinielaTypeConfiguration> QuinielaTypeConfigurations { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Sport> Sports { get; set; }
+        public DbSet<Status> Status { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<TransactionHistory> TransactionHistories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserPicks> UserPicks { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
     }
 }
